a128b680485045a5c113f19089c9a85f37d7cff8
Dhruv suhagiya <dhruv.suhagiya11@gmail.com>
2024-07-13 12:09:50 +0530

    update hr role

--- contanst/data.js
+++ contanst/data.js
@@ -1,4 +1,4 @@
-const userEnum = ['Admin', 'Sales', 'Accountant', 'Dispatcher', 'Production', 'Other'];
+const userEnum = ['Admin', 'Sales', 'Accountant', 'Hr','Dispatcher', 'Production', 'Other'];
 const orderEnum = ['PENDING', 'BILLING', 'DISPATCHING', 'LR PENDING', 'DONE'];
 const companyNameEnum = ['GJ Impex', 'Shreeji sensor', 'Shree Enterprice', 'None'];
 const taskTypeEnum = ['General', 'Query', 'Task']
