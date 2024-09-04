3843523fd91a22b5eda33bce29886b10044c7d88
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-09-04 22:17:13 +0530

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,49 +1,20 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-    // const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization;
 
-    // if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    //     return res.status(401).json({
-    //         status: false,
-    //         data: null,
-    //         message: 'Unauthorized - Token not provided'
-    //     });
-    // }
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({
+            status: false,
+            data: null,
+            message: 'Unauthorized - Token not provided'
+        });
+    }
 
-    // const token = authHeader.split(" ")[1];
+    const token = authHeader.split(" ")[1];
     try {
-        // const decoded = jwt.verify(token, process.env.SECRET);
-        // req.user = decoded;
-        // if (req.baseUrl === '/api/users' && req.user.role !== 'Admin') {
-        //     return res.status(403).json({
-        //         status: false,
-        //         data: null,
-        //         message: 'Forbidden - Only Admins can perform this operation'
-        //     });
-        // }
-
-        // if (
-        //     (req.baseUrl === '/api/orders' && req.method === 'POST' && !['Admin', 'Sales'].includes(req.user.role)) ||
-        //     (req.baseUrl.startsWith('/api/orders/') && req.method === 'DELETE' && !['Admin', 'Sales'].includes(req.user.role))
-        // ) {
-        //     return res.status(403).json({
-        //         status: false,
-        //         data: null,
-        //         message: 'Forbidden - Only Admins or Sales can perform this operation on orders'
-        //     });
-        // }
-
-        // if (
-        //     ((req.baseUrl === '/api/products' || req.baseUrl === '/api/transports') && req.method !== 'GET' && !['Admin', 'Sales', 'Accountant'].includes(req.user.role))
-        // ) {
-        //     return res.status(403).json({
-        //         status: false,
-        //         data: null,
-        //         message: 'Forbidden - Only Admins can perform this operation on products'
-        //     });
-        // }
-
+        const decoded = jwt.verify(token, process.env.SECRET);
+        req.user = decoded;
         next();
     } catch (error) {
         console.error(error);
