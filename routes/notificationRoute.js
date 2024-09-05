192351200915430465adaaac95530ec8c479fccc
Hiren Khunt <hirenkhunt101@gmail.com>
2024-09-05 22:09:06 +0530

    Pagination added in fetch notification

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -3,11 +3,12 @@ const verifyToken = require('../middleware/authMiddleware.js')
 const {
     getAllNotification
 } = require('../controllers/notificationController.js')
+const paginationMiddleware = require('../middleware/paginationMiddleware');
 
 const router = express.Router();
 
 router.use(verifyToken);
 
-router.get('/', getAllNotification);
+router.get('/', paginationMiddleware, getAllNotification);
 
 module.exports = router;
