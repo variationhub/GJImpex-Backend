95561940d4021cf4c2359a12b65d2d5e50c9ca2c
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2024-10-17 10:05:28 +0530

    update

--- services/cronJob.js
+++ services/cronJob.js
@@ -1,10 +1,12 @@
 const cron = require('node-cron');
 const { OrderModel } = require('../models/orderModel');
+const notificationModel = require('../models/notificationModel');
 
-let start = async function() {
+let start = async function () {
     cron.schedule('0 0 * * *', async () => {
         try {
             updateOrderStatusToPast();
+            deleteNotifications();
         } catch (error) {
             console.error('Error in cron job:', error);
         }
@@ -14,9 +16,24 @@ let start = async function() {
 }
 
 let updateOrderStatusToPast = async function () {
-    console.log("cron is running at", new Date());
-    
-    await OrderModel.updateMany({status: "LR PENDING"}, {orderPast: true})
+    try {
+        console.log("cron is running at", new Date());
+
+        await OrderModel.updateMany({ status: "LR PENDING" }, { orderPast: true })
+    } catch (error) {
+        console.error("Error updating order status to past", error);
+    }
+}
+
+let deleteNotifications = async function () {
+    try {
+        const sevenDays = new Date();
+        sevenDays.setDate(sevenDays.getDate() - 7);
+
+        await notificationModel.deleteMany({ createdAt: { $lte: sevenDays } })
+    } catch (error) {
+        console.error("Error deleting notifications", error);
+    }
 }
 
 module.exports = {
